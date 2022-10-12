d7f537a426810b2487e5e0f4683ccb52e44ca769
romulo <romulolespaul@gmail.com>
2022-10-12 09:16:15 -0300

    Changing word "form" for "shape" for better understanding.

--- js-practice/reaction-time/reactionTime.js
+++ js-practice/reaction-time/reactionTime.js
@@ -1,14 +1,14 @@
-var formsList = ["circle", "triangule", "square"];
+var shapesList = ["circle", "triangule", "square"];
 var colorList = ["red", "yellow", "green"];
 var appendSeconds = document.getElementById("seconds");
 var appendMiliseconds = document.getElementById("miliseconds");
 var seconds = 00;
 var miliseconds = 00;
 var interval ;
-var form ;
+var shape ;
 
-function randomForm() {
-    return formsList[Math.floor(Math.random()*formsList.length)];
+function randomShape() {
+    return shapesList[Math.floor(Math.random()*shapesList.length)];
 }
 
 function randomNumber(min, max) {
@@ -18,28 +18,28 @@ function randomNumber(min, max) {
 function start() {
     clearInterval(interval);
     interval = setInterval(startTimer, 10);
-    form = randomForm()
-    if (form == "triangule") {
-        document.getElementById(form).style.borderBottomColor = colorList[Math.floor(Math.random()*colorList.length)];
-        document.getElementById(form).style.marginLeft = randomNumber(0, 650)+"px";
-        document.getElementById(form).style.marginTop = randomNumber(0, 300)+"px";
+    shape = randomShape()
+    if (shape == "triangule") {
+        document.getElementById(shape).style.borderBottomColor = colorList[Math.floor(Math.random()*colorList.length)];
+        document.getElementById(shape).style.marginLeft = randomNumber(0, 650)+"px";
+        document.getElementById(shape).style.marginTop = randomNumber(0, 300)+"px";
     } else {
-        document.getElementById(form).style.backgroundColor = colorList[Math.floor(Math.random()*colorList.length)];
-        document.getElementById(form).style.marginLeft = randomNumber(0, 650)+"px";
-        document.getElementById(form).style.marginTop = randomNumber(0, 300)+"px";
+        document.getElementById(shape).style.backgroundColor = colorList[Math.floor(Math.random()*colorList.length)];
+        document.getElementById(shape).style.marginLeft = randomNumber(0, 650)+"px";
+        document.getElementById(shape).style.marginTop = randomNumber(0, 300)+"px";
     }
 }
 
 function stop() {
     clearInterval(interval);
-    if (form == "triangule") {
-        document.getElementById(form).style.borderBottomColor = "transparent";
-        document.getElementById(form).style.marginLeft = 0+"px";
-        document.getElementById(form).style.marginTop = 0+"px";
+    if (shape == "triangule") {
+        document.getElementById(shape).style.borderBottomColor = "transparent";
+        document.getElementById(shape).style.marginLeft = 0+"px";
+        document.getElementById(shape).style.marginTop = 0+"px";
     } else {
-        document.getElementById(form).style.backgroundColor = "transparent";
-        document.getElementById(form).style.marginLeft = 0+"px";
-        document.getElementById(form).style.marginTop = 0+"px";
+        document.getElementById(shape).style.backgroundColor = "transparent";
+        document.getElementById(shape).style.marginLeft = 0+"px";
+        document.getElementById(shape).style.marginTop = 0+"px";
     }
 }
 
