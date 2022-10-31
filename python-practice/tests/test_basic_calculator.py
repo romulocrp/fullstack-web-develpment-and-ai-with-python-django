96db72a12029b501e29c6448df3622d08953cdf6
romulo <romulolespaul@gmail.com>
2022-10-31 10:15:10 -0300

    Setup object is defined in testing.

--- python-practice/tests/test_basic_calculator.py
+++ python-practice/tests/test_basic_calculator.py
@@ -4,25 +4,21 @@ from basic_calculator import BasicCalculator
 
 class BasicCalculatorTest(unittest.TestCase):
     
-    """@classmethod
-    def setUpClass(cls):
-        bc_obj = BasicCalculator()
-        return bc_obj
+    def setUp(self):
+        self.bc_obj = BasicCalculator()
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        return super().tearDownClass()
+    def tearDown(self):
+        pass
 
 
 
-    @patch("builtins.input", lambda _: "y")"""
+    """@patch("builtins.input", lambda _: "y")"""
     def test_calculation(self):
-        bc_obj = BasicCalculator()
-        bc_obj.name = "romulo"
-        bc_obj.a = 3
-        bc_obj.b = 4
-        bc_obj.c = 5
-        result_message = bc_obj.print_results()
+        self.bc_obj.name = "romulo"
+        self.bc_obj.a = 3
+        self.bc_obj.b = 4
+        self.bc_obj.c = 5
+        result_message = self.bc_obj.print_results()
         self.assertEqual("Your name is: romulo\n\
 The numbers you inputed are: 3.00, 4.00 and, 5.00\n\
 The result is: 35.00", result_message)
