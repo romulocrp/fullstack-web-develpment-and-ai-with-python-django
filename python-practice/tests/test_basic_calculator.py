ef046b1e58108eda89762f3acd961359ee593308
romulo <romulolespaul@gmail.com>
2022-11-01 16:46:04 -0300

    Refactored user input, mocked user input for testing.

--- python-practice/tests/test_basic_calculator.py
+++ python-practice/tests/test_basic_calculator.py
@@ -7,18 +7,17 @@ class BasicCalculatorTest(unittest.TestCase):
     def setUp(self):
         self.bc_obj = BasicCalculator()
 
-    def tearDown(self):
-        pass
-
-
-
-    """@patch("builtins.input", lambda _: "y")"""
     def test_calculation(self):
-        self.bc_obj.name = "romulo"
-        self.bc_obj.a = 3
-        self.bc_obj.b = 4
-        self.bc_obj.c = 5
+        self.input_mocking("romulo", "3 4 5")
         result_message = self.bc_obj.print_results()
         self.assertEqual("Your name is: romulo\n\
 The numbers you inputed are: 3.00, 4.00 and, 5.00\n\
 The result is: 35.00", result_message)
+
+    def input_mocking(self, inp1: str, inp2: str):
+        """Using a context manager patch method is useful in the sense that there are more than
+        one input inside each test, creating a method for it makes the inputing reusable and readable."""
+        with patch("builtins.input", return_value=inp1):
+            self.bc_obj.user_input_name()
+        with patch("builtins.input", return_value=inp2):
+            self.bc_obj.user_input_values()
