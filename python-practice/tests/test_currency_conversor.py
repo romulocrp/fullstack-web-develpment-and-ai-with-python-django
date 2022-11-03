ca85a50ee5904f3c3ce5514205073ff0ad288800
romulo <romulolespaul@gmail.com>
2022-11-03 09:54:44 -0300

    Positive integer test completed.

--- python-practice/tests/test_currency_conversor.py
+++ python-practice/tests/test_currency_conversor.py
@@ -1,14 +1,22 @@
 import unittest
+from datetime import date
 from unittest.mock import patch
 from currency_conversor import CurrencyConverter
 
 class CurrencyConversorTest(unittest.TestCase):
+
+    request_moment = date.today()
 
     def setUp(self):
         self.cc_obj = CurrencyConverter()
 
     def test_amounts(self):
         """Testing positive integer value."""
+        self.input_mocking("200", "ars")
+        result_message_1 = self.cc_obj.print_converted_amount()
+        expected_message_1 = f"The converted amount is: 30165.40 ARS at\
+ {CurrencyConversorTest.request_moment.month}/{CurrencyConversorTest.request_moment.day}/{CurrencyConversorTest.request_moment.year}."
+        self.assertEqual(expected_message_1, result_message_1)
 
     def input_mocking(self, inp1: str, inp2: str):
         """Using a context manager patch method is useful in the sense that there are more than
